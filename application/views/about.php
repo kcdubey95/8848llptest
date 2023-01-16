1316cd5199b13d94bc46555efe625bf5983e4e5b
kcdubey95 <dubey.krishnachand@gmail.com>
2023-01-17 00:45:59 +0530

    new about page

--- application/views/about.php
+++ application/views/about.php
@@ -5,7 +5,7 @@
                 About Us
             </div>
             <div class="card-body">
-                <p class="card-text">WIt believes that ethical management is not only a tool for responding to the rapid changes in the global business environment but also a vehicle for building trust with its various stakeholders including customers, shareholders, employees, business partners, and local communities. With an aim to become one of the most ethical companies In the world, - continues to train its employees and operate monitoring systems, while practicing fair and transparent corporate management.</p>
+                <p class="card-text">We believes that ethical management is not only a tool for responding to the rapid changes in the global business environment but also a vehicle for building trust with its various stakeholders including customers, shareholders, employees, business partners, and local communities. With an aim to become one of the most ethical companies In the world, - continues to train its employees and operate monitoring systems, while practicing fair and transparent corporate management.</p>
             </div>
         </div>
     </div>
